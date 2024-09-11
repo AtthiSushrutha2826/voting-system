b14b05e55fce01996e30eb3666d1c57545628da7
JRBros2346 <jrbros2346@gmail.com>
2024-09-11 18:07:44 +0530

    [Add] Dummy Blockchain

--- app/blockchain.py
+++ app/blockchain.py
@@ -1,1 +1,26 @@
-" " 
+hasher = lambda data: data+'*'
+
+class Block:
+    def __init__(self, data, hash, lastHash) -> None:
+        self.data = data
+        self.hash = hash
+        self.lastHash = lastHash
+    def __repr__(self) -> str:
+        return f'[{self.lastHash}] {self.data} ({self.hash})'
+
+class Blockchain:
+    def __init__(self) -> None:
+        self.chain = [Block('gen-data', 'gen-hash', 'gen-lastHash')]
+
+    def addBlock(self, data):
+        self.chain.append(Block(data, hasher(data), self.chain[-1].hash))
+    
+    def __repr__(self) -> str:
+        return '\n'.join([c.__repr__() for c in self.chain])
+
+fooBlockChain = Blockchain()
+fooBlockChain.addBlock('one')
+fooBlockChain.addBlock('two')
+fooBlockChain.addBlock('three')
+
+print(fooBlockChain)
